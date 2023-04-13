e00e0d377d89133b7e2268c4dc25b8ff55675d2a
Polina Igrushkina <polina.igrushkina@gmail.com>
2023-04-13 11:07:41 +0300

    clear comments

--- js/slider_above_the_sea.js
+++ js/slider_above_the_sea.js
@@ -1,11 +1,9 @@
-// выбираем все нужные элементы
 const container = document.querySelector(".aboveTheSea__container");
 const image = document.querySelector(".aboveTheSea__image");
 const prevButton = document.querySelector(".prev-button");
 const nextButton = document.querySelector(".next-button");
 const sliderNumber = document.querySelector(".slider-number");
 
-// создаем массив из путей к картинкам
 const images = [
   "images/above_the_sea.jpg",
   "images/above_the_sea2.jpg",
@@ -13,13 +11,11 @@ const images = [
 ];
 let currentImage = 0;
 
-// функция для обновления картинки и номера слайда
 function updateImage() {
   image.src = images[currentImage];
   sliderNumber.textContent = `${currentImage + 1}/${images.length}`;
 }
 
-// функция для перехода к предыдущей картинке
 function prevImage() {
   currentImage--;
   if (currentImage < 0) {
@@ -28,7 +24,6 @@ function prevImage() {
   updateImage();
 }
 
-// функция для перехода к следующей картинке
 function nextImage() {
   currentImage++;
   if (currentImage >= images.length) {
@@ -37,9 +32,7 @@ function nextImage() {
   updateImage();
 }
 
-// добавляем слушатели событий на кнопки
 prevButton.addEventListener("click", prevImage);
 nextButton.addEventListener("click", nextImage);
 
-// отображаем номер первого слайда при загрузке страницы
 updateImage();
